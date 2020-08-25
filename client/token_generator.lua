03a761956eb89bf1cef9b33cac6f5a94bae0266d
Rubylium <alexisoko0@gmail.com>
2020-08-25 12:48:15 +0200

    Client: Save token into local

--- client/token_generator.lua
+++ client/token_generator.lua
@@ -1,7 +1,10 @@
+local generated_token = nil
+
 function GenerateToken()
     return ""..math.random(10001,90009).."-"..math.random(10001,90009).."-"..math.random(10001,90009).."-"
 end
 
 Citizen.CreateThreadNow(function()
-    TriggerServerEvent("AC:RegisterClient", GenerateToken())
+    generated_token = GenerateToken()
+    TriggerServerEvent("AC:RegisterClient", generated_token)
 end)
