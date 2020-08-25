ca656d30589476a31d149f7981babdcc4618eb33
Rubylium <alexisoko0@gmail.com>
2020-08-25 16:52:16 +0200

    Config: Adding entity blacklist + rate limiter

--- config.lua
+++ config.lua
@@ -12,6 +12,29 @@ alive_check_cooldown = 1 -- In minutes.
 modules = {
     healths_check = true, -- This disable health regen
     spectator = true,
+    blacklist_entity = true,
+    entity_rate_limiter = true,
+    entity_rate_limiter_limit = 300,
+    entity_rate_limiter_reset = 5, -- In second. 30 means the limiter will be reseted every 30 seconds.
+    entity_rate_limiter_alerts = { -- Display console alerts
+        [50] = true, 
+        [100] = true, 
+        [150] = true, 
+        [200] = true, 
+        [250] = true, 
+        [300] = true, 
+        [350] = true, 
+        [400] = true, 
+        [450] = true, 
+        [500] = true, 
+    }
+}
+
+blacklisted_entity = {
+    [GetHashKey("ambulance")] = true,
+    [GetHashKey("police")] = true,
+    [GetHashKey("police2")] = true,
+    [GetHashKey("policeb")] = true,
 }
 
 violations = {
