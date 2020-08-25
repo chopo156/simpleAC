81f8d684de895d39284c8e3b5bf35dc00b8f649b
Rubylium <alexisoko0@gmail.com>
2020-08-25 12:53:32 +0200

    Server: Check players restricted acces on connect

--- server/restricte_handler.lua
+++ server/restricte_handler.lua
@@ -5,16 +5,31 @@ function RestrictPlayer(id)
     for k,v in pairs(ids) do
         if not restricte_ip then
             if not string.find(v, "ip:") then
-                table.insert(restricted, v)
+                restricted[v] = true
                 if display_console_restrict_add then
                     print("^1"..prefix.."^7: ["..v.."] has been restricted.")
                 end
             end
         else
-            table.insert(restricted, v)
+            restricted[v] = true
             if display_console_restrict_add then
                 print("^1"..prefix.."^7: ["..v.."] has been restricted.")
             end
         end
     end
 end
+
+
+local function OnPlayerConnecting(name, setKickReason, deferrals)
+    local source = source
+    local identifiers = GetPlayerIdentifiers(player)
+
+    for _, v in pairs(identifiers) do
+        if restricted[v] ~= nil then
+            CancelEvent()
+            setKickReason(prefix.." "..restricted_message)
+        end
+    end
+end
+
+AddEventHandler("playerConnecting", OnPlayerConnecting)
