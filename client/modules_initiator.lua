2c6256402fdfa0835be0cd6c19e382e840cba35e
Rubylium <alexisoko0@gmail.com>
2020-08-25 16:04:22 +0200

    Update modules_initiator.lua

--- client/modules_initiator.lua
+++ client/modules_initiator.lua
@@ -3,4 +3,7 @@ AddEventHandler("AC:InitModules", function(modules)
     if modules.healths_check then
         InitHealthChecks()
     end
+    if modules.spectator then
+        InitSpectatorChecks()
+    end
 end)
