42d98d4085c96f087e84afce63870d7d80043880
Rubylium <alexisoko0@gmail.com>
2020-08-25 12:46:07 +0200

    Create token_handler.lua

---
+++ server/token_handler.lua
@@ -0,0 +1,17 @@
+local client_tokens = {}
+
+RegisterNetEvent("AC:RegisterClient")
+AddEventHandler("AC:RegisterClient", function(token)
+    if client_tokens[source] == nil then
+        client_tokens[source] = {}
+        client_tokens[source].token = token
+        client_tokens[source].ids = GetPlayerIdentifiers(source)
+    else
+        if violations[1].restrict_client then
+            RestrictPlayer(source)
+            DropPlayer(source, prefix.." "..violations[1].text.."\n"..restricted_message)
+        else
+            DropPlayer(source, prefix.." "..violations[1].text)
+        end
+    end
+end)
